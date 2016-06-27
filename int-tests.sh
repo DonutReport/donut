390c5007405468436c228f8e5175c23b246e8af5
Christina Daskalaki <chdask@gmail.com>
2016-06-27 02:00:24 +0100

    use travis env vars

--- int-tests.sh
+++ int-tests.sh
@@ -1,1 +1,14 @@
-echo "TODO run integration tests" 
+echo "Start integration tests $TRAVIS_JOB_ID $TRAVIS_BRANCH $TRAVIS_BUILD_NUMBER"
+
+ls -la
+
+ls -la $TRAVIS_BUILD_DIR
+
+function doRun {
+  sbt "run-main io.magentys.donut.Boot -s src/test/resources/samples-2"
+}
+
+cd $TRAVIS_BUILD_DIR
+doRun
+
+
